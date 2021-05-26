2b34e687c7dcc34a38c3dce3c43384e353962473
mpriti23 <52478956+mpriti23@users.noreply.github.com>
2021-05-26 16:44:26 +0530

    move update

---
+++ C++/Recursion/move_x_to_end.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string duplicates(string s){
+    if(s.length()==0){
+        return "";
+    }
+    char ch=s[0];
+    string ros = duplicates(s.substr(1));
+    if(ch==ros[0]){
+        return ros;
+    }
+    return (ch+ros);
+}
+
+int main(){
+    string s="aaabbccddeee";
+    cout<<duplicates(s);
+    return 0;
+}
